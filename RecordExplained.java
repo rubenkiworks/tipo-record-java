9cac0f6831e2ae8339253c20651a073fdfc372da
rubenkiworks <rubengomezkiworks@gmail.com>
2024-11-20 15:53:54 +0100

    sin record

--- RecordExplained.java
+++ RecordExplained.java
@@ -1,3 +1,28 @@
+// el tipo record de java 
+
+import java.time.LocalDate;
+
+class Persona{
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private Genero genero;
+
+    enum Genero {
+        HOMBRE, MUJER, OTRO
+    }
+    
+    public Persona(){}
+    
+    @Override
+    public String toString(){
+        return super.toString();
+    }
+
+    
+}
+
+
+
 public class RecordExplained {
     
     public static void main(String[] args) {
